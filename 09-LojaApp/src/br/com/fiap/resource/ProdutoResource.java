5e84eb9d94e7114c2bc98561de4d5ea9ed93069a
Rafael Matsuyama <rafael.matsuyama@gmail.com>
2019-03-14 06:37:31 -0300

    Exercício 09 - WebService RESTFul Básico
    Utilização do Jersey para criação de um WebService RESTFul básico.

---
+++ 09-LojaApp/src/br/com/fiap/resource/ProdutoResource.java
@@ -0,0 +1,15 @@
+package br.com.fiap.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/produto")
+public class ProdutoResource {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String buscar() {
+		return "Ola Mundo!";
+	}
+}
